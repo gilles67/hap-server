c959aef2821f163436e0653fc1271a294a5b3c5f
gilles67 <gilles@easyiteam.net>
2020-10-02 20:14:56 +0200

    manage on message error

--- happower/mqtt.py
+++ happower/mqtt.py
@@ -9,25 +9,28 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("hap/alsa/#")
 
 def on_message(client, userdata, msg):
-    payload = msg.payload.decode('UTF-8')
-    if msg.topic == "hap/power/audio/set":
-        setAudioPower(payload)
-    if msg.topic == "hap/power/ampli/set":
-        setAmpliPower(payload)
-    if msg.topic == "hap/power/led/set":
-        pled.setLed(payload)
-    if msg.topic == "hap/power/set":
-        if payload == "On":
-            pmanage.OnSequence()
-        if payload == "Off":
-            pmanage.OffSequence()
-    if msg.topic == "hap/power/button/action":
-        if payload == "short press":
-            pmanage.TogglePower()
-    if msg.topic == "hap/alsa/volume/set":
-        setAlsaVolume(payload, client)
-
-    print(msg.topic+" "+str(msg.payload))
+    try:
+        payload = msg.payload.decode('UTF-8')
+        if msg.topic == "hap/power/audio/set":
+            setAudioPower(payload)
+        if msg.topic == "hap/power/ampli/set":
+            setAmpliPower(payload)
+        if msg.topic == "hap/power/led/set":
+            pled.setLed(payload)
+        if msg.topic == "hap/power/set":
+            if payload == "On":
+                pmanage.OnSequence()
+            if payload == "Off":
+                pmanage.OffSequence()
+        if msg.topic == "hap/power/button/action":
+            if payload == "short press":
+                pmanage.TogglePower()
+        if msg.topic == "hap/alsa/volume/set":
+            setAlsaVolume(payload, client)
+    except Exception as err:
+        print(err)
+    finally:
+        print(msg.topic+" "+str(msg.payload))
 
 
 client = mqtt.Client()
